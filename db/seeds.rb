303a71f7cd4e2f119136cddc4a83e22254ed451b
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-07-13 22:42:09 +0900

    データ試作

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times.each do |i|
+  Post.create(title: "test#{i + 1}")
+end
