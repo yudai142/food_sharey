eefd8e45f2a2c8f30405d97b290c2bfea2504559
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-08-03 11:57:59 +0900

    slim導入

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'sorcery'
+gem 'slim-rails'
+gem 'html2slim'
--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   purge: [
     "./app/**/*.html.erb",
+    "./app/**/*.js.erb",
+    "./app/**/*.html.slim",
+    "./app/**/*.js.slim",
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
     "./app/javascript/**/*.vue"
