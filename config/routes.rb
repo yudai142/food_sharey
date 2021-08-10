481800d8343c46a39b131dff9c34c6fde824c32f
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-08-10 13:10:29 +0900

    ヘッダーリンク作成&ルートパス変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails
   .application
   .routes
   .draw do
-    root 'users#new'
+    root 'foods#index'
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
