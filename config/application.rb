bf35f577b5ecc4d393dcfb2ed38058ad3b8def4d
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-08-10 13:04:35 +0900

    enumを使ったカテゴリ選択実装

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
