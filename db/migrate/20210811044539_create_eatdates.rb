964750e7d2383490e28a227dda9c43d3f58cc0f2
yudai142 <yudai.shimoji.42865233@gmail.com>
2021-08-13 18:35:23 +0900

    eat_timeのデータ型の訂正

--- db/migrate/20210811044539_create_eatdates.rb
+++ db/migrate/20210811044539_create_eatdates.rb
@@ -3,7 +3,7 @@ class CreateEatdates < ActiveRecord::Migration[6.1]
     create_table :eatdates do |t|
       t.date  :date, null: false, index: true
       t.integer :timezone, null: false, index: true
-      t.timestamp :eat_time
+      t.time :eat_time
       t.text :comment
       t.references :user, foreign_key: true, null: false
 
