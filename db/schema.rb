aa14429f1b1480cd786adc3b285ca6c39e2aa037
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-08-16 11:51:17 +0900

    マイグレーション訂正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_131738) do
+ActiveRecord::Schema.define(version: 2021_08_13_095720) do
 
   create_table "eatdates", charset: "utf8mb4", force: :cascade do |t|
     t.date "date", null: false
