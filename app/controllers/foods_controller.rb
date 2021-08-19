761a989dee3a10768be688ba28b5c844d72bc1ef
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-08-19 14:14:04 +0900

    食事情報の記録処理実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,10 +7,12 @@ class FoodsController < ApplicationController
 
   def create
     params[:timezone] = params[:eatdate][:timezone]
-    @eatdate = Eatdate.where(date: params[:date], timezone: params[:timezone], user_id: current_user.id)
-    @eatdate.present? ? @eatdate.update(eat_time: params[:eat_time],comment: params[:comment]) : Eatdate.create!(eatdate_params)
-    @mymenu = Mymenu.find(params[:mymenu_id])
-    @food = Food.create!(name: @mymenu.name, image: @mymenu.image, calorie: @mymenu.calorie, protein: @mymenu.protein, fat: @mymenu.fat, carbohydrate: @mymenu.carbohydrate, sugar: @mymenu.sugar, dietary_fiber: @mymenu.dietary_fiber, salt: @mymenu.salt, Vitamin_A: @mymenu.Vitamin_A, Vitamin_D: @mymenu.Vitamin_D, Vitamin_E: @mymenu.Vitamin_E, Vitamin_B1: @mymenu.Vitamin_B1, Vitamin_B2: @mymenu.Vitamin_B2, Vitamin_B6: @mymenu.Vitamin_B6, Vitamin_B12: @mymenu.Vitamin_B12, Vitamin_C: @mymenu.Vitamin_C, potassium: @mymenu.potassium, calcium: @mymenu.calcium, magnesium: @mymenu.magnesium, iron: @mymenu.iron, eatdate_id: params[:mymenu_id])
+    @eatdate = Eatdate.find_by(date: params[:date], timezone: params[:timezone], user_id: current_user.id)
+    @eatdate.present? ? @eatdate.update(eat_time: params[:eat_time],comment: params[:comment]) : @eatdate = Eatdate.create!(eatdate_params)
+    if params[:mymenu_id].present?
+      @mymenu = Mymenu.find(params[:mymenu_id])
+      @food = Food.create!(name: @mymenu.name, image: @mymenu.image.url, calorie: @mymenu.calorie, protein: @mymenu.protein, fat: @mymenu.fat, carbohydrate: @mymenu.carbohydrate, sugar: @mymenu.sugar, dietary_fiber: @mymenu.dietary_fiber, salt: @mymenu.salt, Vitamin_A: @mymenu.Vitamin_A, Vitamin_D: @mymenu.Vitamin_D, Vitamin_E: @mymenu.Vitamin_E, Vitamin_B1: @mymenu.Vitamin_B1, Vitamin_B2: @mymenu.Vitamin_B2, Vitamin_B6: @mymenu.Vitamin_B6, Vitamin_B12: @mymenu.Vitamin_B12, Vitamin_C: @mymenu.Vitamin_C, potassium: @mymenu.potassium, calcium: @mymenu.calcium, magnesium: @mymenu.magnesium, iron: @mymenu.iron, eatdate_id: @eatdate.id)
+    end
     redirect_to new_food_path
     return
   end
