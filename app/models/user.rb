7fe53e85582e62e51f579e51effaa4c11c4b86c6
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-09-27 14:27:21 +0900

    ユーザー新規登録ページ　バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :email, uniqueness: true, format: {with: VALID_EMAIL_REGEX}, presence: true
   validates :password,length: {minimum: 3},if: -> { new_record? || changes[:crypted_password] }
   validates :password,confirmation: true,if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation,presence: true,if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
   has_many :eatdates
   has_many :mymenus
 end
