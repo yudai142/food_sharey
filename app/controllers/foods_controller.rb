54a3d8bc25fdb77ce979ec7a31c819e7b203f2a2
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-06-20 14:13:05 +0900

    ログアウト中でもランキング表示

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,20 +4,24 @@ class FoodsController < ApplicationController
     @date = Time.now
     if (Time.parse("03:00")..Time.parse("09:59")).cover? @date
       @ranking = Eatdate.where(timezone: 1).includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
-      @menu = "朝食"
+      @menu = "人気の朝食メニュー"
     elsif (Time.parse("10:00")..Time.parse("14:59")).cover? @date
       @ranking = Eatdate.where(timezone: 2).includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
-      @menu = "昼食"
+      @menu = "人気の昼食メニュー"
     elsif (Time.parse("15:00")..Time.parse("23:59")).cover? @date or (Time.parse("00:00")..Time.parse("02:59")).cover? @date
       @ranking = Eatdate.where(timezone: 4).includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
-      @menu = "夕食"
+      @menu = "人気の夕食メニュー"
     end
     @eatdate = Array.new
     @ranking.each do |ranking|
       if Food.find_by(eatdate_id: ranking)
         @eatdate.push(ranking)
       end
-      break if @eatdate.length == 10
+      if logged_in?
+        break if @eatdate.length == 10
+      else
+        break if @eatdate.length == 8
+      end
     end
     if logged_in?
       @morning_id = Eatdate.find_by(date: @date,timezone: 1 , user_id: current_user.id)
