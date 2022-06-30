4e951dba654d434ea3b3246cfd39b2c6924b8209
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-07-01 07:40:08 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,7 @@ class UsersController < ApplicationController
     (redirect_to root_path, notice: '既にログインしています'; return) if logged_in?
     @user = User.new(user_params)
     if @user.save!
-      redirect_to login_path
-      flash[:notice] = 'ユーザーの作成に成功しました'
+      redirect_to login_path, notice: 'ユーザーの作成に成功しました'
     else
       flash.now[:alert] = 'ユーザーの作成に失敗しました'
       render :new
