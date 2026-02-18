e019e29ffe1b9aae505e330967b517a7bdea7dcd
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-02-18 11:38:26 +0900

    CarrierWave修正

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Set secret_key_base from environment variable for production
+if Rails.env.production? && ENV['SECRET_KEY_BASE'].present?
+  Rails.application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,18 +3,26 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id:  ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
-    }
+  # Use S3 only if AWS credentials are available
+  if ENV['AWS_ACCESS_KEY_ID'].present? && ENV['AWS_SECRET_ACCESS_KEY'].present?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+      config.fog_provider = 'fog/aws'
+      config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id:  ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        region: 'ap-northeast-1'
+      }
 
-    config.fog_directory  = ENV['AWS_ACCESS_FOG_DIRECTORY']
-    config.asset_host = ENV['AWS_ACCESS_ASSET_HOST']
+      config.fog_directory  = ENV['AWS_ACCESS_FOG_DIRECTORY']
+      config.asset_host = ENV['AWS_ACCESS_ASSET_HOST']
+    end
+  else
+    # Fallback to local file storage if AWS credentials are not set
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
   end
 
 else
