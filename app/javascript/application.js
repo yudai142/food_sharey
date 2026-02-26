eac9285cddfb13916a2bf987595c08171e5c7e7d
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-02-26 23:07:08 +0900

    修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,6 @@ import * as ActiveStorage from '@rails/activestorage'
 import './channels'
 import './entrypoints/resize.js'
 import './entrypoints/preview.js'
-// CSS is built separately by build-css.mjs and copied to app/assets/builds/
 
 Rails.start()
 ActiveStorage.start()
