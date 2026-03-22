7ab58e789bc364c3065e155d1177e8755aa88d5c
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-03-22 09:10:17 +0900

    Remove unsupported import.meta.glob from channels - use esbuild compatible approach

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,4 +1,5 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-import.meta.glob('./**/*_channel.js', { eager: true })
+// Note: import.meta.glob is not supported by esbuild
+// Channels will be imported manually as needed
